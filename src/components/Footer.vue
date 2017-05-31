a72fb2a52470fa5f2019be810d8bab734b469fac
Luke Pettway <luke.pettway@thinkbrownstone.com>
2017-05-31 13:48:38 -0400

    Created a basic app to show content

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+      <p>{{footerText}}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'appFooter',
+  data () {
+    return {
+      footerText: 'This is the footer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+footer {
+    background: #34495e;
+    color: #fff;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'appHeader',
+  data () {
+    return {
+      title: 'Title'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+header {
+    background: #34495e;
+    color: #fff;
+}
+
+h1, h2 {
+  font-weight: normal;
+  margin: 0;
+}
+
+
+</style>
